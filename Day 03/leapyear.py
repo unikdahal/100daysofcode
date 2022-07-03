4132eec48f0c245eac8f960dff37a575e9e50a25
Unik-Dahal <unikdahal03@gmail.com>
2022-07-03 11:44:33 +0530

    Leap Year

---
+++ Day 03/leapyear.py
@@ -0,0 +1,11 @@
+#Leap Year Calculator
+
+year=int(input('Enter the year'))
+
+if(year%4==0):
+    if(year%100==0 and year%400!=0):
+        print('Not a leap year')
+    else:
+        print(f'{year} is a leap year')
+else:
+    print('Not a leap year')
