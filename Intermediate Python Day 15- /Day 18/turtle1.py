f9043c17e1b9555bee6860c946506f5de0c04adc
Unik-Dahal <unikdahal03@gmail.com>
2022-08-03 02:05:09 +0530

    Quiz Game

--- Intermediate Python Day 15- /Day 18/turtle1.py
+++ Intermediate Python Day 15- /Day 18/turtle1.py
@@ -1,42 +1,31 @@
 import turtle
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen
 import random
 
-timmy=Turtle()
+timmy = Turtle()
 
 turtle.colormode(255)
 
+
 def random_color():
-    r=random.randint(0,255)
+    r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    random_color=(r,g,b)
+    random_color = (r, g, b)
     return random_color
+
 
 turtle.speed(0)
 
+
 def draw_spirograph(size_of_graph):
-    for i in range(int(360/size_of_graph)):
+    for i in range(int(360 / size_of_graph)):
         timmy.color(random_color())
         timmy.circle(100)
-        timmy.setheading(timmy.heading()+size_of_graph)
+        timmy.setheading(timmy.heading() + size_of_graph)
+
 
 draw_spirograph(1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-screen=Screen()
+screen = Screen()
 screen.exitonclick()
